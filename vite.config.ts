68626766867a1e7f99cd013847f74e44a5790391
Anoop Rao G N <anoopginigini@gmail.com>
2024-06-10 22:05:46 +0530

    vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import fs from "fs";
+// import fs from "fs";
 import path from "path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
@@ -13,10 +13,10 @@ export default defineConfig({
         }
     },
     server: {
-        port: 3000,
-        https: {
-            key: fs.readFileSync("../cert/key.pem"), // replace it with your key path
-            cert: fs.readFileSync("../cert/cert.pem") // replace it with your certificate path
-        }
+        port: 3000
+        // https: {
+        //     key: fs.readFileSync("../cert/key.pem"), // replace it with your key path
+        //     cert: fs.readFileSync("../cert/cert.pem") // replace it with your certificate path
+        // }
     }
 });
