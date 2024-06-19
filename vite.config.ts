edf59cee30baad5ad685bedb396eb4a758e4461b
Anoop Rao G N <anoopginigini@gmail.com>
2024-06-20 00:21:18 +0530

    build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
-import fs from "fs";
+// import fs from "fs";
 import path from "path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
+
 dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         mkcert({
-            savePath: "../certs", // save the generated certificate into certs directory
-            force: true // force generation of certs even without setting https property in the vite config
+            // savePath: "../certs", // save the generated certificate into certs directory
+            // force: true // force generation of certs even without setting https property in the vite config
         })
     ],
     resolve: {
@@ -23,10 +25,10 @@ export default defineConfig({
         "process.env": process.env
     },
     server: {
-        port: 3000,
-        https: {
-            key: fs.readFileSync("../cert/key.pem"), // replace it with your key path
-            cert: fs.readFileSync("../cert/cert.pem") // replace it with your certificate path
-        }
+        // port: 3000,
+        // https: {
+        //     key: fs.readFileSync("../cert/key.pem"), // replace it with your key path
+        //     cert: fs.readFileSync("../cert/cert.pem") // replace it with your certificate path
+        // }
     }
 });
